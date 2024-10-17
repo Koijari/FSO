dd5d208397d6d1c645dc08364a4029a9f9d6e150
Janne Kolari <j.m.kolari@outlook.com>
2024-10-17 14:33:50 +0300

    1.5 retry

--- Osa1/kurssitiedot/src/App.jsx
+++ Osa1/kurssitiedot/src/App.jsx
@@ -1,9 +1,9 @@
 
-const Header = (props) => {
+const Header = ({ course }) => {
   //console.log(props)
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{course.name}</h1>
     </>
   )
 }
@@ -17,23 +17,23 @@ const Part = (props) => {
   );
 };
 
-const Content = ({ parts }) => {
+const Content = ({ course }) => {
   //console.log(parts)
   return (
     <>
-      <Part name={parts[0].name} exercises={parts[0].exercises}/>
-      <Part name={parts[1].name} exercises={parts[1].exercises}/>
-      <Part name={parts[2].name} exercises={parts[2].exercises}/>
+      <Part name={course.parts[0].name} exercises={course.parts[0].exercises}/>
+      <Part name={course.parts[1].name} exercises={course.parts[1].exercises}/>
+      <Part name={course.parts[2].name} exercises={course.parts[2].exercises}/>
     </>
   )
 }
 
-const Total = ({parts}) => {
+const Total = ({ course }) => {
   //console.log(parts)
   
   return (
     <>
-      <p>Number of exercises {parts[0].exercises+parts[1].exercises+parts[2].exercises}</p>
+      <p>Number of exercises {course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises}</p>
     </>
   )
 }
@@ -62,12 +62,12 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course.name}/>
+      <Header course={course}/>
 
-      <Content  parts={course.parts}
+      <Content  course={course}
       />
 
-      <Total parts={course.parts} />
+      <Total course={course} />
     </div>
   )
 }
