26da4a7f0787a47495cf06e1a67fde257deb296c
Janne Kolari <j.m.kolari@outlook.com>
2024-11-07 14:01:14 +0200

    2.14

--- Osa2/puhelinluettelo/src/App.jsx
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -1,20 +1,68 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import personService from './services/persons'
+
+
+const Filter = ({ filterText, handleFilterChange }) => {
+  return (
+    <>
+      {<p>filter shown with <input value={ filterText } onChange={ handleFilterChange }/></p>} 
+    </>
+  )
+}
+
+const PersonForm = (props) => {
+  //console.log(props)
+  return(
+    <>
+      <form onSubmit={props.addName}>
+        <div>
+          name: <input value={props.newName || ''} onChange={props.handleName} />
+          <br />
+          number: <input value={props.newNumber || ''} onChange={props.handleNumber} />
+        </div>
+        <>
+          <button type={props.submit}>add</button>
+        </>
+      </form>
+    </>
+  )
+}
+
+const Persons = ({ namesToShow, deleteName }) => {
+  //console.log(namesToShow)
+  return (
+    <>    
+      {namesToShow.map(person => (
+        <p key={person.id}>{person.name} {person.number} <button onClick={() => deleteName(person.id) }>delete</button></p>
+      ))}
+      
+    </>
+  )
+}
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Robert Ludlum', number: '050-05050505' },
-    { name: 'Wile E. Coyote', number: '555-34-BeebBeeb' },
-    { name: 'Karaoke Maestro', number: '045-SingALong' },
-    { name: 'Scott Mariner', number: '555-Bujah' }
-  ])
 
+  const [persons, setPersons] = useState([])
+  //console.log(persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
 
+  const namesToShow = persons.filter(person => 
+    person.name.toLowerCase().includes(filterText.toLowerCase())
+  )
+
+  useEffect(() => {
+    personService
+    .getAll()
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
+
   const handleName = (event) => {
-    setNewName(event.target.value)
+    setNewName(event.target.value)    
   }
 
   const handleNumber = (event) => {
@@ -25,61 +73,59 @@ const App = () => {
     setFilterText(event.target.value)
   }
 
-  const addName = (event) => {
+  const addNameNumber = (event) => {
     event.preventDefault()
-    const name = {
+    const nameObj = {
       name: newName,
       number: newNumber
-    }
-    const namesInBook = persons.map(person => person.name)
-    if (!(namesInBook.includes(newName))) {
-      setPersons(persons.concat(name))           
-    } else {
-      alert(`${newName} is already added to phonebook`)
-    }
-    setNewName('')
-    setNewNumber('')
+  }
+      
+  const namesInBook = persons.map(person => person.name)
+  //console.log(namesInBook)
+  if (!(namesInBook.includes(newName))) {
+    personService
+    .create(nameObj)
+    .then(response => {
+      setPersons(persons.concat(response.data))
+    })
+    .catch(error => console.log('Luonti epäonnistui', error)
+    )
+  } else {
+    alert(`${newName} is already added to phonebook`)
+  }
+  setNewName('')
+  setNewNumber('')
   }
 
-  const namesToShow = persons.filter(person => 
-    person.name.toLowerCase().includes(filterText.toLowerCase())
-  )
+  const deleteName = (id) => {
+    //console.log(persons)
+    const nameToDelete = persons.find(person => person.id === id)
+    if (window.confirm(`Delete ${nameToDelete.name}`)) {
+      personService
+        .delName(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+        })
+        .catch(error => {
+          console.log('Poisto epäonnistui', error)
+        })
+    }
+  }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <div>
-      filter shown with 
-        <input 
-          value={filterText} 
-          onChange={handleFilterChange} 
-        />
-      </div><br />
-      <form onSubmit={addName}>
-        <div>
-          name: <input 
-            value={newName}
-            onChange={handleName}
-          /><br />
-          number: <input
-            value={newNumber}
-            onChange={handleNumber}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {namesToShow.map(person => 
-          <p key={person.name}>{ person.name } {person.number}</p>
-        )}
-      </ul>
+      <h1>Phonebook</h1>      
+      <Filter filterText={ filterText } handleFilterChange={ handleFilterChange }/>
+
+      <h3>Add a new</h3>
+      <PersonForm addName={ addNameNumber } newName={ newName } handleName={ handleName } 
+        newNumber={ newNumber } handleNumber={ handleNumber } submit={'submit'} />
+
+      <h3>Numbers</h3>
+      <Persons namesToShow={ namesToShow } deleteName={ deleteName }/>
       
     </div>
   )
-
 }
 
 export default App
